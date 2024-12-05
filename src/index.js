a599959cfd9284136523e8c1df6bb0dd0361425f
Sara Tijari <sara.tijari0001@gmail.com>
2024-12-05 21:42:47 +0100

    initial try

--- src/index.js
+++ src/index.js
@@ -1,13 +1,66 @@
-var express = require('express');
-var app = express ();
+var express = require('express'); 
 const path = require('path');
-const connectDB = require('./server')
-connectDB();
-app.listen(8080);
-console.log('http://localhost:8080/');
-app.set("view engine", "ejs");
-app.use('/static', express.static(path.join(__dirname, 'static')));
+const bcrypt = require("bcrypt")
+const collection = require("./server")
 
-app.get('/', async (req, res) => {
-    res.render('principale');
+
+var app = express (); 
+ 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.set('view engine', 'ejs')
+app.use(express.static("static"));
+
+app.get("/login", (req, res) =>{
+    res.render("login");
+})
+
+app.get("/signup", (req, res) =>{
+    res.render("signup");
+})
+
+
+app.post("/signup", async(req,res) => {
+    const data ={
+        gmail: req.body.gmail,
+        password: req.body.password
+    }
+    const exitinguser = await collection.findOne({gmail: data.gmail});
+
+    if (exitinguser){
+        res.send("Gmail already exists, please try with an other Gmail")
+    }else{
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(data.password, saltRounds)
+
+    data.password = hashedPassword; 
+
+    const userdata = await collection.insertMany(data);
+    console.log(userdata);
+
+    res.render("home");
+    }
 });
+app.post("/login", async(req, res)=>{
+        try{
+            const checked = await collection.findOne({gmail: req.body.gmail});
+            if(!checked){
+                res.send("Gmail not found");
+            }
+            const isPasswordMatch = await bcrypt.compare(req.body.password, checked.password)
+            if(isPasswordMatch){
+                res.render("home")
+            }else{
+                res.send("wrong password, please try again!")
+            }
+        }catch{
+            res.status(500).send("An error occurred, please try again later.");
+
+        }
+})
+
+const port = 5000
+app.listen(port, async() =>{
+    console.log(`Server runnung on Port, ${port}`)
+})
--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
-const url = 'mongodb://localhost:27017/marketplace';
 const mongoose = require('mongoose');
-const connectDB = async () => {
-    try {
-        await mongoose.connect(url, {
-        });
-        console.log('OK');
-    } catch (err) {
-        console.error('PAS OK:', err);
-        process.exit(1);
+const connect = mongoose.connect("mongodb://localhost:27017/Login")
+
+connect.then(()=> {
+    console.log("Database connected Successfully");
+})
+.catch(()=>{
+    console.log("DB can't be connected")
+});
+
+const LogInSchema = new mongoose.Schema({
+    gmail: {
+        type: String,
+        required : true,
+        unique : true,
+        validate: {
+            validator: function (v) {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email address!`
+        }
+    },
+    password: {
+        type: String,
+        required : true
     }
-};
-module.exports = connectDB;
+});
+
+const collection = new mongoose.model("users", LogInSchema);
+
+module.exports = collection;
